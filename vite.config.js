4cef142ed97ef6817f81daea07f959bef9d760e8
Seterium <seterium@yandex.ru>
2021-11-07 17:18:31 +0200

    pwa fix

--- vite.config.js
+++ vite.config.js
@@ -19,22 +19,22 @@ const pwaConfig = {
     theme_color: '#ffffff',
     icons: [
       {
-        src: '/pwa/icon-192x192.png',
+        src: '/w-calendar/pwa/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png'
       },
       {
-        src: '/pwa/icon-256x256.png',
+        src: '/w-calendar/pwa/icon-256x256.png',
         sizes: '256x256',
         type: 'image/png'
       },
       {
-        src: '/pwa/icon-384x384.png',
+        src: '/w-calendar/pwa/icon-384x384.png',
         sizes: '384x384',
         type: 'image/png'
       },
       {
-        src: '/pwa/icon-512x512.png',
+        src: '/w-calendar/pwa/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png'
       }
